79ed030213e77a16439f03ed43dc52586aaf511b
catay-strk <catay.stark@gmail.com>
2026-08-18 13:59:39 +0200

    write first variable

--- Learn JavaScript/Build a Passenger Counter App/index.js
+++ Learn JavaScript/Build a Passenger Counter App/index.js
@@ -1,1 +1,9 @@
-document.getElementById("count-el").innerText = 5
+// document.getElementById("count-el").innerText = 5
+
+let count = 0
+
+console.log(count)
+
+let myAge = 24
+
+console.log(myAge)
