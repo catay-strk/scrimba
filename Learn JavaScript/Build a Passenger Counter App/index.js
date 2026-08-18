5f10346b0f31126d26869be3b497c7c0750da34d
catay-strk <catay.stark@gmail.com>
2026-08-18 16:14:52 +0200

    Use plus equal for count

--- Learn JavaScript/Build a Passenger Counter App/index.js
+++ Learn JavaScript/Build a Passenger Counter App/index.js
@@ -2,11 +2,11 @@ let countEL = document.getElementById("count-el")
 let count = 0
 
 function increment() {
-    count = count + 1
+    count += 1
     countEL.innerText = count
 }
 
-// 1. Create a function, save(), which logs out the count when it's called
 function save() {
     console.log(count)
 }
+
