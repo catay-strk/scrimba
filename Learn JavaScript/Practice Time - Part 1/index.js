8d48413f67c014b3b97ce36d3c44cb0eb101fd36
catay-strk <catay.stark@gmail.com>
2026-08-18 17:29:05 +0200

    Concentrate two strings in a function

--- Learn JavaScript/Practice Time - Part 1/index.js
+++ Learn JavaScript/Practice Time - Part 1/index.js
@@ -5,3 +5,14 @@ let lastName = "Stark"
 let fullName = firstName + " " + lastName
 // Log fullName to the console
 console.log(fullName)
+
+let name = "Linda"
+let greeting = "Hi there"
+
+// Create a function that logs out "Hi there, Linda!" when called
+
+function greet() {
+    console.log(greeting + ", " + name + "!")
+}
+
+greet()
