401e0a74cf629628b611696ee110cd4056b1d71f
catay-strk <catay.stark@gmail.com>
2026-08-18 14:06:00 +0200

    Basic mathematical operations

--- Learn JavaScript/Build a Passenger Counter App/index.js
+++ Learn JavaScript/Build a Passenger Counter App/index.js
@@ -1,9 +1,15 @@
 // document.getElementById("count-el").innerText = 5
 
-let count = 0
+// le firstBatch = 5
+// let secondBatch = 7
+// let count = firstBatch + secondBatch
+// console.log(count)
 
-console.log(count)
+// 1. Create two variables, myAge and humanDogRatio
+// 2. Multiply the two together and store the result in myDogAge 
+// 3. Log myDogAge to the console
 
 let myAge = 24
-
-console.log(myAge)
+let humanDogRatio = 7
+let myDogAge = myAge * humanDogRatio
+console.log(myDogAge)
