dafafcb727d7974c928728ebd42d25dcfddd3504
catay-strk <catay.stark@gmail.com>
2026-08-19 13:09:05 +0200

    Add the hasBlackJack and isAlive variable

--- Learn JavaScript/Build a Blackjack Game/index.js
+++ Learn JavaScript/Build a Blackjack Game/index.js
@@ -6,20 +6,23 @@
 
 let firstCard = 6 // getRandomIntInclusive(2, 11)
 let secondCard = 9 // getRandomIntInclusive(2, 11)
-
 let sum = firstCard + secondCard
+let hasBlackJack = false
+// 1. Create a variable called isAlive and assign it to true
+let isAlive = true
 
-console.log(sum)
-// Write the conditional according to these rules:
-
-// if less than or equal to 20 -> "Do you want to draw a new card? 🙂"
-// else if exactly 21 -> "Wohoo! You've got Blackjack! 🥳"
-// else -> "You're out of the game! 😭"
+// 2. Flip its value to false in the appropriate code block
 if (sum <= 20)
 {
     console.log("Do you want to draw a new card? 🙂")
 } else if (sum === 21) {
     console.log("Wohoo! You've got Blackjack! 🥳")
+    hasBlackJack = true
 } else {
     console.log("You're out of the game! 😭")
+    isAlive = false
 }
+// 3. Log it out to check that you're doing it right
+// CASH OUT!
+console.log(hasBlackJack)
+console.log(isAlive)
