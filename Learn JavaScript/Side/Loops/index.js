1517081b62cfea874cc1972a2f5164c041fbc5e4
catay-strk <catay.stark@gmail.com>
2026-08-19 14:37:28 +0200

    Aside: For loops and arrays

--- Learn JavaScript/Side/Loops/index.js
+++ Learn JavaScript/Side/Loops/index.js
@@ -12,3 +12,19 @@ for ( let count = 10;  count < 21;  count += 1 )  {
     console.log(count)
 
 }
+
+// loops and arrays
+
+let messages = [
+    "Hey, how's it going?",        
+    "I'm great, thank you! How about you?",
+    "All good. Been working on my portfolio lately.",
+    "Same here!",
+    "Great to hear",
+    "🙌"
+]
+
+
+for (let i = 0; i < messages.length; i++) {
+    console.log(messages[i])
+}
