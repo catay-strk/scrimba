a2aeb512fe9532c696034835f4a9c7c03f97a036
catay-strk <catay.stark@gmail.com>
2026-08-19 12:58:40 +0200

    If...else statement revamp

--- Learn JavaScript/Build a Blackjack Game/index.js
+++ Learn JavaScript/Build a Blackjack Game/index.js
@@ -4,15 +4,18 @@
 //   return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled); // The maximum is inclusive and the minimum is inclusive
 // }
 
-// 1. Create two variables, firstCard and secondCard. 
-// Set their values to a random number between 2-11
 let firstCard = 6 // getRandomIntInclusive(2, 11)
 let secondCard = 9 // getRandomIntInclusive(2, 11)
-// 2. Create a variable, sum, and set it to the sum of the two cards
+
 let sum = firstCard + secondCard
 
 console.log(sum)
-if (sum < 21)
+// Write the conditional according to these rules:
+
+// if less than or equal to 20 -> "Do you want to draw a new card? 🙂"
+// else if exactly 21 -> "Wohoo! You've got Blackjack! 🥳"
+// else -> "You're out of the game! 😭"
+if (sum <= 20)
 {
     console.log("Do you want to draw a new card? 🙂")
 } else if (sum === 21) {
