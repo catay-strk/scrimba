be74b3a0db5a1a1697d0aad24bd366c657d0b5cd
catay-strk <catay.stark@gmail.com>
2026-08-19 14:23:38 +0200

    Aside arrays.push() and .pop()

--- Learn JavaScript/Side/Arrays/index.js
+++ Learn JavaScript/Side/Arrays/index.js
@@ -30,3 +30,31 @@ console.log(experience[0])
 // It should contain your name (string), your age (number), and whether you like pizza (boolean)
 
 let totallyMe = ["Catay Stark", 24, true]
+
+// .push() and .pop()
+
+let cards = [7, 4]
+
+
+cards.push(6)
+
+console.log(cards)
+
+// Push the newMessage to the messages array, and then log out the array
+
+let messages = [
+    "Hey, how's it going?",        
+    "I'm great, thank you! How about you?",
+    "All good. Been working on my portfolio lately."
+]
+
+let newMessage = "Same here!"
+
+messages.push(newMessage)
+
+console.log(messages)
+
+// How can you remove the last item in an array? Try to google it!
+
+messages.pop()
+console.log(messages)
