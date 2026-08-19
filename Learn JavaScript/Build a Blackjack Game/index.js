fa7436956557655ccb8fc228116e6e6804648922
catay-strk <catay.stark@gmail.com>
2026-08-19 12:49:48 +0200

    If..else conditionals

--- Learn JavaScript/Build a Blackjack Game/index.js
+++ Learn JavaScript/Build a Blackjack Game/index.js
@@ -12,3 +12,11 @@ let secondCard = 9 // getRandomIntInclusive(2, 11)
 let sum = firstCard + secondCard
 
 console.log(sum)
+if (sum < 21)
+{
+    console.log("Do you want to draw a new card? 🙂")
+} else if (sum === 21) {
+    console.log("Wohoo! You've got Blackjack! 🥳")
+} else {
+    console.log("You're out of the game! 😭")
+}
