e721014a5e2e97fe408ff17bc4b712553935d81b
catay-strk <catay.stark@gmail.com>
2026-08-19 13:55:49 +0200

    Display the cards

--- Learn JavaScript/Build a Blackjack Game/index.js
+++ Learn JavaScript/Build a Blackjack Game/index.js
@@ -11,12 +11,13 @@ let hasBlackJack = false
 let isAlive = true
 let message = ""
 let messageEl = document.getElementById("message-el")
-// 2. Store the sum paragraph in a variable called sumEl
-//let sumEL = document.getElementById("sum-el")
-let sumEL = document.querySelector("#sum-el")
+let sumEL = document.getElementById("sum-el")
+// 2. Store the cards paragraph in a variable called cardsEl
+let cardsEl = document.getElementById("cards-el")
 
 function startGame() {
-    // 3. Render the sum on the page using this format -> "Sum: 14"
+    // 3. Render the cars on the page using this format -> "Cards: 10 4"
+    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
     sumEL.textContent = "Sum: " + sum
     if (sum <= 20){
         message = "Do you want to draw a new card?"
