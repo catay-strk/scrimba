51e62cbebfa1b2bc1cad78d198ed6823f3ce97ba
catay-strk <catay.stark@gmail.com>
2026-08-20 11:03:08 +0200

    Logical operators

--- Learn JavaScript/Practice Time - Part 2/index.js
+++ Learn JavaScript/Practice Time - Part 2/index.js
@@ -71,3 +71,14 @@ largeCountries.shift()
 largeCountries.unshift("China")
 
 console.log(largeCountries)
+
+// logical operators
+let dayOfMonth = 31
+let weekday = "Friday"
+
+// If it is Friday the 13th, log out this spooky face: 😱
+// Use the logical "AND operator" -> &&
+
+if (dayOfMonth === 13 && weekday === "Friday") {
+    console.log("😱")
+}
