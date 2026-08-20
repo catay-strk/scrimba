de109615a324d3373a3b858492a4a7b14f5dd92a
catay-strk <catay.stark@gmail.com>
2026-08-20 10:52:14 +0200

    if else

--- Learn JavaScript/Practice Time - Part 2/index.js
+++ Learn JavaScript/Practice Time - Part 2/index.js
@@ -14,3 +14,27 @@ function logData() {
 }
 // Call the logData() function to verify that it works
 logData()
+
+// if else
+let age = 15
+
+// less than 6 years old -> free
+// 6 to 17 years old     -> child discount
+// 18 to 26 years old    -> student discount
+// 27 to 66 years old    -> full price
+// over 66 years old     -> senior citizen discount
+
+// Create a conditional statement (if/else/else if) that logs out the discount
+// the passenger will get based upon the value of the age variable
+
+if (age < 6) {
+    console.log("free")
+} else if (age <= 17) {
+    console.log("child discount")
+} else if (age <= 26) {
+    console.log("student discount")
+} else if (age <= 66) {
+    console.log("full price")
+} else {
+    console.log("senior citizen discount")
+}
