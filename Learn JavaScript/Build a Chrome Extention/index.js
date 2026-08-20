d5ad9c53e4a5ca5394b4616709155df3ac8cbf9c
catay-strk <catay.stark@gmail.com>
2026-08-20 23:34:50 +0200

    Use createElement() and append() instead of innerHTML

--- Learn JavaScript/Build a Chrome Extention/index.js
+++ Learn JavaScript/Build a Chrome Extention/index.js
@@ -2,7 +2,7 @@ let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
 
 const inputEL = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
-const unEl = document.getElementById("ul-el")
+const ulEl = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEL.value)
@@ -11,5 +11,9 @@ inputBtn.addEventListener("click", function() {
 
 // Replace .textContent with .innerHTML and use <li> tags
 for (let i = 0; i < myLeads.length; i++) {
-    unEl.innerHTML += "<li>" + myLeads[i] + "</li>"
+    ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
+    // altenative Method:
+    const li = document.createElement("li")
+    li.textContent = myLeads[i]
+    ulEl.append(li)
 }
