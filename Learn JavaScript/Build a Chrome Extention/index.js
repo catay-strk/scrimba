dd7e1b77cca0c803b9eccd9725241c5067fab6b2
catay-strk <catay.stark@gmail.com>
2026-08-20 23:12:04 +0200

    Create the unordered list

--- Learn JavaScript/Build a Chrome Extention/index.js
+++ Learn JavaScript/Build a Chrome Extention/index.js
@@ -2,13 +2,14 @@ let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
 
 const inputEL = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
+// 2. Grab the unordered list and store it in a const variable called ulEl
+const unEl = document.getElementById("un-el")
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEL.value)
     console.log(myLeads)
 })
 
-// Log out the items in the myLeads array using a for loop 
 for (let i = 0; i < myLeads.length; i++) {
     console.log(myLeads[i])
 }
