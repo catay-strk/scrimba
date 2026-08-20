9577bb947117710a7e63b453c00af22916d942be
catay-strk <catay.stark@gmail.com>
2026-08-20 11:00:26 +0200

    push, pop, unshift, shift challange

--- Learn JavaScript/Practice Time - Part 2/index.js
+++ Learn JavaScript/Practice Time - Part 2/index.js
@@ -55,3 +55,19 @@ The 5 largest countries in the world:
 for (let i = 0; i < largeCountries.length; i++) {
     console.log("- " + largeCountries[i])
 }
+
+// push, pop, unshift, shift challange
+let largeCountries = ["Tuvalu","India","USA","Indonesia","Monaco"]
+
+// You need to help me fixup the largeCountries array so that 
+// China and Pakistan are added back into their respective places
+
+// Use push() & pop() and their counterparts unshift() & shift()
+// Google how to use unshift() and shift()
+
+largeCountries.pop()
+largeCountries.push("Pakistan")
+largeCountries.shift()
+largeCountries.unshift("China")
+
+console.log(largeCountries)
