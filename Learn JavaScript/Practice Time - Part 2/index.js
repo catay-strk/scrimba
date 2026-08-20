dd3e19c5a63f90417533b5040f8864664a837f2b
catay-strk <catay.stark@gmail.com>
2026-08-20 10:46:50 +0200

    Objects and functions

---
+++ Learn JavaScript/Practice Time - Part 2/index.js
@@ -0,0 +1,16 @@
+// Objects and functions
+// Create a person object that contains three keys: name, age, and county.
+// Use yourself as an example to set the values for name, age, and country
+let person = {
+    name: "Catay",
+    age: 24,
+    country: "Earth"
+}
+// Create a function, logData(), that uses the person object to create a
+// string in the following format: 
+// "Per is 35 years old and lives in Norway"
+function logData() {
+        console.log(person.name + " is " + person.age + " years old an lives on " + person.country)
+}
+// Call the logData() function to verify that it works
+logData()
