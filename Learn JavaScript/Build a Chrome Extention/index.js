aaa00c430d9b77bc8d17e4cb5650ff1d55c42a4d
catay-strk <catay.stark@gmail.com>
2026-08-20 22:50:02 +0200

    Refaactor to addEventListener

--- Learn JavaScript/Build a Chrome Extention/index.js
+++ Learn JavaScript/Build a Chrome Extention/index.js
@@ -1,4 +1,5 @@
-// Log out "Button clicked!" when the user clicks the "SAVE INPUT" button
-function saveLead() {
-    console.log("Button clicked!")
-}
+let inputBtn = document.getElementById("input-btn")
+
+inputBtn.addEventListener("click", function() {
+    console.log("Button clicked from addEventListener")
+})
