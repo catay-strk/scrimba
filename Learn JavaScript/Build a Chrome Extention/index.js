8bd01e0676d94cc525f3e015faaaeaeb1b9657ec
catay-strk <catay.stark@gmail.com>
2026-08-20 23:15:54 +0200

    Render the leads in the unordered list

--- Learn JavaScript/Build a Chrome Extention/index.js
+++ Learn JavaScript/Build a Chrome Extention/index.js
@@ -3,13 +3,15 @@ let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
 const inputEL = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 // 2. Grab the unordered list and store it in a const variable called ulEl
-const unEl = document.getElementById("un-el")
+const unEl = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEL.value)
     console.log(myLeads)
 })
 
+// Render the leads in the unordered list using ulEl.textContent
 for (let i = 0; i < myLeads.length; i++) {
     console.log(myLeads[i])
+    unEl.textContent += myLeads[i] + " "
 }
