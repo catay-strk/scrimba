407274e37954469fc070ecf3bd9242d0d60e0828
catay-strk <catay.stark@gmail.com>
2026-08-20 11:18:02 +0200

    EmojiFighter

---
+++ Learn JavaScript/Practice Time - Part 2/EmojiFighter/index.js
@@ -0,0 +1,16 @@
+let fighters = ["🐉", "🐥", "🐊","💩", "🦍", "🐢", "🐩", "🦭", "🦀", "🐝", "🤖", "🐘", "🐸", "🕷","🐆", "🦕", "🦁"]
+
+let stageEl = document.getElementById("stage")
+let fightButton = document.getElementById("fightButton")
+
+fightButton.addEventListener("click", function() {
+    // Challenge:
+    // When the user clicks on the "Pick Fighters" button, pick two random 
+    // emoji fighters and display them as i.e. "🦀 vs 🐢" in the "stage" <div>.
+    console.log("click")
+
+    let fighterOne = fighters[Math.floor(Math.random() * fighters.length)]
+    let fighterTwo = fighters[Math.floor(Math.random() * fighters.length)]
+
+    stageEl.textContent = fighterOne + " vs " + fighterTwo
+})
