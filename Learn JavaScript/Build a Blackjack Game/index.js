2d1ffcae198b8a4796c4f6c93d521df787f5e19e
catay-strk <catay.stark@gmail.com>
2026-08-20 10:24:22 +0200

    Use an object to store player data

--- Learn JavaScript/Build a Blackjack Game/index.js
+++ Learn JavaScript/Build a Blackjack Game/index.js
@@ -1,4 +1,8 @@
-
+// 2. Create the player object. Give it two keys, name and chips, and set their values
+let player = {
+    name: "dude",
+    chips: 5
+}
 let cards = []
 let sum = 0
 let hasBlackJack = false
@@ -7,7 +11,11 @@ let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEL = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
+// 3. Grab ahold of the player-el paragraph and store it in a variable called playerEl
+let playerEL = document.getElementById("player-el")
+// 4. Render the player's name and chips in playerEl
 
+playerEL.innerText = player.name + ": $" + player.chips
 
 function getRandomCard() {
     let number = Math.floor(Math.random() * 13) + 1
