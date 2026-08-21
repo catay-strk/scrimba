64b7b5c631dffbf33eddb73b3482afae8608cc80
catay-strk <catay.stark@gmail.com>
2026-08-21 13:47:14 +0200

    How function parameters can improve our code

--- Learn JavaScript/Build a Chrome Extention/index.js
+++ Learn JavaScript/Build a Chrome Extention/index.js
@@ -1,42 +1,25 @@
 let myLeads = []
-
+let oldLeads = []
 const inputEL = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
-// 1. Store the delete button in a deleteBtn variable
 const deleteBtn = document.getElementById("delete-btn")
 
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
-    renderLeads()
+    render(myLeads)
 }
 
-// 2. Listen for double clicks on the delete button (google it!)
-// 3. When clicked, clear localStorage, myLeads, and the DOM
-deleteBtn.addEventListener("dblclick", function() {
-    localStorage.clear()
-    myLeads = []
-    renderLeads()
-})
-
-inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEL.value)
-    console.log(myLeads)
-    inputEL.value = ""
-    localStorage.setItem("myLeads", JSON.stringify(myLeads))
-    renderLeads()
-})
-
-function renderLeads() {
+function render(leads) {
     let listItems = ""
     
-    for (let i = 0; i < myLeads.length; i++) {
+    for (let i = 0; i < leads.length; i++) {
         listItems += `
             <li>
-                <a target='_blank' href='${myLeads[i]}'>
-                    ${myLeads[i]}
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
                 </a>
             </li>`
         console.log(listItems)
@@ -48,3 +31,17 @@ function renderLeads() {
     
     ulEl.innerHTML = listItems
 }
+
+deleteBtn.addEventListener("dblclick", function() {
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+})
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEL.value)
+    console.log(myLeads)
+    inputEL.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    render(myLeads)
+})
