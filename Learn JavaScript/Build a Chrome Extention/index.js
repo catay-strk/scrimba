bdd642045f0fd599a364e3df7ff853f10c5e89e2
catay-strk <catay.stark@gmail.com>
2026-08-21 12:50:03 +0200

    Your first localStorage

--- Learn JavaScript/Build a Chrome Extention/index.js
+++ Learn JavaScript/Build a Chrome Extention/index.js
@@ -4,6 +4,20 @@ const inputEL = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
+// 1. Save a key-value pair in localStorage
+// 2. Refresh the page. Get the value and log it to the console
+// 3. Clear localStorage
+
+// HINTS:
+// localStorage.setItem(key, value)
+// localStorage.getItem(key)
+// localStorage.clear()
+// PS: both key and value need to be strings
+
+localStorage.setItem("myLeads", "www.examplelead.com")
+console.log(localStorage.getItem("myLeads"))
+localStorage.clear()
+
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEL.value)
     console.log(myLeads)
