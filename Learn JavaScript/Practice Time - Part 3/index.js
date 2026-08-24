863f690ed65192c7b46b22dc872a11b897d67ffa
catay-strk <catay.stark@gmail.com>
2026-08-24 15:42:58 +0200

    Generate sentence

--- Learn JavaScript/Practice Time - Part 3/index.js
+++ Learn JavaScript/Practice Time - Part 3/index.js
@@ -43,3 +43,25 @@ log(myCourses)
 
 //localStorage.setItem("SaveStr", "Hello there!")
 console.log(localStorage.getItem("SaveStr"))
+
+// Generate sentence
+// The generateSentence(desc, arr) takes two parameterer: a description and an array.
+// It should return a string based upon the description and array.
+
+// Example 1: if you pass in "largest countries",and ["China", "India", "USA"],
+// it should return the string: "The 3 largest countries are China, India, USA"
+
+// Example 2: If you pass in "best fruits" and ["Apples", "Bananas"], it should return:
+// "The 2 best fruits are Apples, Bananas"
+
+// Use both a for loop and a template string to solve the challenge
+function generateSentence(desc, arr) {
+    let arrContent = arr[0]
+    for (let i = 1; i < arr.length; i++) {
+        arrContent += ", " + arr[i]
+    }
+    return `The ${arr.length} ${desc} are ${arrContent}`
+}
+
+console.log(generateSentence("largest countries", ["China", "India", "USA"]))
+console.log(generateSentence("best fruits", ["Apples", "Bananas"]))
