47146ad247189cec1da5206501c394bc0c9cf93b
catay-strk <catay.stark@gmail.com>
2026-08-24 16:11:32 +0200

    Convert string to number challenge

--- Learn JavaScript/Practice Time - Part 3/rounding numbers/index.js
+++ Learn JavaScript/Practice Time - Part 3/rounding numbers/index.js
@@ -1,14 +1,11 @@
 // Challenge:
-// Round the price in the button down to two decimal places.
-// Don't know which method to use? Google it!
+// The toFixed() method doesn't work anymore. Can you make it work?
+// Google the error message if you're unsure about how to do it
 
-const totalPrice = 420.69235632455
-const btn = document.getElementById("purchase-btn")
-btn.textContent = `Buy €${ totalPrice.toFixed(2) }`
+const totalPrice = "420.69235632455"
+const btn = document.getElementById("buy-btn")
+btn.textContent = `Buy €${ Number(totalPrice).toFixed(2) }`
 
-// btn.textContent = `Buy €${ roundToTwo(totalPrice) }`
-
-// function roundToTwo(num) {
-//     num = Math.round(num * 100) / 100
-//     return num
-// }
+// const totalPrice = parseFloat("420.69235632455")
+// const btn = document.getElementById("buy-btn")
+// btn.textContent = `Buy €${ totalPrice.toFixed(2) }`
