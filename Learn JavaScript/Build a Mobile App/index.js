945f5ac0c7b77e55c4214ba38ba9d4dac1bde153
catay-strk <catay.stark@gmail.com>
2026-08-25 18:23:29 +0200

    Deploy to Netlify

--- Learn JavaScript/Build a Mobile App/index.js
+++ Learn JavaScript/Build a Mobile App/index.js
@@ -1,6 +1,6 @@
 import PocketBase from "https://cdn.jsdelivr.net/npm/pocketbase/dist/pocketbase.es.mjs"
 
-const pb = new PocketBase("http://127.0.0.1:8090") // this doesn't make sense for client side code
+const pb = new PocketBase("http://192.168.178.21:8090") // this doesn't make sense for client side code
 
 const referenceInDB = pb.collection("leads")
 
