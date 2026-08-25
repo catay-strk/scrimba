220c2b4e6d185e70b1e86b57733abb14e91fc5e6
catay-strk <catay.stark@gmail.com>
2026-08-25 13:46:20 +0200

    Delete unneeded code

--- Learn JavaScript/Build a Mobile App/index.js
+++ Learn JavaScript/Build a Mobile App/index.js
@@ -1,31 +1,14 @@
 import PocketBase from "https://cdn.jsdelivr.net/npm/pocketbase/dist/pocketbase.es.mjs"
 
-const pb = new PocketBase("http://127.0.0.1:8090") // thi doesn't make sense for client side code
+const pb = new PocketBase("http://127.0.0.1:8090") // this doesn't make sense for client side code
 
-console.log(pb)
+// Challenge: Delete the code associated with the save tab button
+// Challenge: Delete all of the localStorage code
 
-let myLeads = []
-let oldLeads = []
 const inputEL = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 const deleteBtn = document.getElementById("delete-btn")
-const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
-const tabBtn = document.getElementById("tab-btn")
-
-if (leadsFromLocalStorage) {
-    myLeads = leadsFromLocalStorage
-    render(myLeads)
-}
-
-tabBtn.addEventListener("click", function() {
-
-    chrome.tabs.query({active:true, currentWindow: true}, function(tabs) {
-        myLeads.push(tabs[0].url)
-        localStorage.setItem("myLeads", JSON.stringify(myLeads))
-        render(myLeads)
-    })
-})
 
 function render(leads) {
     let listItems = ""
@@ -48,15 +31,10 @@ function render(leads) {
 }
 
 deleteBtn.addEventListener("dblclick", function() {
-    localStorage.clear()
-    myLeads = []
-    render(myLeads)
+
 })
 
 inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEL.value)
-    console.log(myLeads)
+    console.log(inputEL.value)
     inputEL.value = ""
-    localStorage.setItem("myLeads", JSON.stringify(myLeads))
-    render(myLeads)
 })
