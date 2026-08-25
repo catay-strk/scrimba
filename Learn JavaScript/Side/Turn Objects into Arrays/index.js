c68f69a19f133021f04fe69152288c985af39e86
catay-strk <catay.stark@gmail.com>
2026-08-25 15:47:26 +0200

    Aside: Turn Objects into an Array

---
+++ Learn JavaScript/Side/Turn Objects into Arrays/index.js
@@ -0,0 +1,9 @@
+const loginCredentials = {
+    "rafidhoda": "BestPassword123",
+    "shahrukhkhan": "InBigCitiesSmallThingsHappen",
+    "jackblack": "ThisIsNotTheGreatestPasswordInTheWorld"
+}
+
+console.log(Object.keys(loginCredentials))
+console.log(Object.values(loginCredentials))
+console.log(Object.entries(loginCredentials)) // [[keys, values]]
