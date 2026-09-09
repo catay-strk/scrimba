3b1bbf576d0df2ab677bd5460e0fd38adf5062ce
catay-strk <catay.stark@gmail.com>
2026-09-09 12:46:48 +0200

    Recreate the server

--- Learn_Node.js/Build a REST API/server.js
+++ Learn_Node.js/Build a REST API/server.js
@@ -1,7 +1,16 @@
-console.log("We have a package.json!!!")
+import http from 'node:http'
+
+const PORT = 8000
+
+const server = http.createServer((req, res) => {
+    res.end("Server here^^")
+})
+
+server.listen(PORT, () => {console.log(`Server is listening on port: ${PORT}`)})
 
 /*
 Challenge:
-1. Create a package.json file by typing "npm init" in the terminal.
-2. Answer the questions using the info in project_details.md.
+1. Recreate our server so we send a string over http when a GET request comes in. 
+2. Test it by making a GET request to http://localhost:8000 in the network widget.
+See hint.md for prompts.
 */
