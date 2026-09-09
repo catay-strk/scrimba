a3463ddd43295b82bb80734c1580dd6170eaa478
catay-strk <catay.stark@gmail.com>
2026-09-09 13:25:12 +0200

    Adding Content-Type

--- Learn_Node.js/Build a REST API/server.js
+++ Learn_Node.js/Build a REST API/server.js
@@ -6,16 +6,17 @@ const PORT = 8000
 const server = http.createServer(async (req, res) => {
   console.log(req.url)
 
-/*
-Challenge:
-  1. Store our data in a const ‘destinations’.
-  2. When a GET request is received to the ‘/api' endpoint, send our JSON stringified data.
-    Think: What changes will you need to make to get this to work?
-*/
     const destinations = await getDataFromDB()
 
     if (req.url === "/api" && req.method === "GET") {
-
+/*
+Challenge:
+1. Access the ‘setHeader’ method on the response object and pass in two strings to set the      
+   Content-Type to ‘application/json’ - watch out for casing! 
+2. Access the 'statusCode' property and set it to 200.
+*/
+        res.setHeader("Content-Type", "application/json")
+        res.statusCode = 200
         res.write(JSON.stringify(destinations))
     }
     res.end()
