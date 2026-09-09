eab0ac5f9848c81e551154f4c3ecdc914826abf2
catay-strk <catay.stark@gmail.com>
2026-09-09 13:03:23 +0200

    Routing and the req object

--- Learn_Node.js/Build a REST API/server.js
+++ Learn_Node.js/Build a REST API/server.js
@@ -3,14 +3,24 @@ import http from 'node:http'
 const PORT = 8000
 
 const server = http.createServer((req, res) => {
-    res.end("Server here^^")
-})
-
-server.listen(PORT, () => {console.log(`Server is listening on port: ${PORT}`)})
+  console.log(req.url)
 
 /*
 Challenge:
-1. Recreate our server so we send a string over http when a GET request comes in. 
-2. Test it by making a GET request to http://localhost:8000 in the network widget.
-See hint.md for prompts.
+Check the ‘url’ property on the req object. 
+Only serve our string if it’s ‘/api’.
 */
+/*
+Challenge: 
+1. Check the ‘method’ property on the req object.
+   Only serve our string if it’s ‘GET’.
+*/
+
+    if (req.url === "/api" && req.method === "GET") {
+
+        res.write("Server here^^")
+    }
+    res.end()
+})
+
+server.listen(PORT, () => {console.log(`Server is listening on port: ${PORT}`)})
