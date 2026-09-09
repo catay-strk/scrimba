83a33e1b196cdbdc6ab0197db0ff3d63f970b415
catay-strk <catay.stark@gmail.com>
2026-09-09 20:42:53 +0200

    Explainer: Adding POST

--- Learn_Node.js/Build From the other Side/server.js
+++ Learn_Node.js/Build From the other Side/server.js
@@ -1,22 +1,26 @@
 import http from 'node:http'
 import { serveStatic } from './utils/serveStatic.js'
-import { handleGet } from './handlers/routeHandlers.js'
+import { handleGet, handlePost } from './handlers/routeHandlers.js'
+
 
 const PORT = 8000
 
 const __dirname = import.meta.dirname
 
 const server = http.createServer(async (req, res) => {
-/*
-Challenge: 
-   1. Set up a route for ‘/api’.
-   2. Nest an if to check if the method is ‘GET’. 
-   2. When a GET request is received to '/api', use handleGet() to handle it.
-*/ 
+
     if (req.url.startsWith('/api')) {
         if (req.method === 'GET') {
             return await handleGet(res)
+        } 
+        else if (req.method === 'POST') {
+            return handlePost(req, res)
         }
+        /*
+        Challenge: 
+        1. Add a route for a POST request to '/api'.
+        2. When a request comes in, pass the req and res to handlePost().
+        */
 
     } else if (!req.url.startsWith('/api')) {
 
