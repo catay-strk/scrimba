ea8998f47b9e09b74b969b40011486ee64818ed7
catay-strk <catay.stark@gmail.com>
2026-09-09 13:55:22 +0200

    Add Path Parameters

--- Learn_Node.js/Build a REST API/server.js
+++ Learn_Node.js/Build a REST API/server.js
@@ -5,7 +5,7 @@ import { error } from 'node:console'
 const PORT = 8000
 
 const server = http.createServer(async (req, res) => {
-  console.log(req.url)
+    console.log(req.url)
 
     const destinations = await getDataFromDB()
 
@@ -13,17 +13,45 @@ const server = http.createServer(async (req, res) => {
         res.setHeader("Content-Type", "application/json")
         res.statusCode = 200
         res.write(JSON.stringify(destinations))
-    } else {
+    } else if (req.url.startsWith("/api/continent") && req.method === "GET") {
         /*
         Challenge:
-        1. If the client tries to access a route that isn’t covered by the above, send this object: 
-            {error: "not found", message: "The requested route does not exist"}
-        Think: what do we need to send along with the data?
+        1. Check if the url starts with “/api/continent”.
+            (Is there a JS method that allows you to check what a string starts with?)
+
+        2. If it does, serve only items from that continent.
+            (How can you get to what comes after the final slash?)
+            (What method can you use to filter data?)
         */
-       res.setHeader("Content-Type", "application/json")
-       // res.statusMessage = "error"
-       res.statusCode = 404
-       res.end(JSON.stringify({error: "not found", message: "The requested route does not exist"}))
+        const continent = req.url.split("/").pop()
+
+        const filterdData = []
+
+        for (let i = 0; i < destinations.length; i++) {
+            if (destinations[i].continent.toLowerCase() === continent.toLowerCase()) {
+                filterdData.push(destinations[i])
+            }
+        }
+
+        /* alternative to the for loop / if:
+            const filteredData = destinations.filter((destination) => {
+            return destination.continent.toLowerCase() === continent.toLowerCase()
+            })
+        */
+
+        res.setHeader("Content-Type", "application/json")
+        res.statusCode = 200
+        res.end(JSON.stringify(filterdData))
+
+    } else {
+
+        res.setHeader("Content-Type", "application/json")
+        // res.statusMessage = "error"
+        res.statusCode = 404
+        res.end(JSON.stringify({
+            error: "not found", 
+            message: "The requested route does not exist"
+        }))
     }
     res.end()
 })
