65cf2d99c55cb4cfa9745552d5c7723fe383c66e
catay-strk <catay.stark@gmail.com>
2026-09-09 17:01:18 +0200

    Get Path to Resource

--- Learn_Node.js/Build From the other Side/server.js
+++ Learn_Node.js/Build From the other Side/server.js
@@ -1,33 +1,23 @@
-/*
-Challenge: 
-1. Initialise a nodejs project:
-	Name: “from-the-other-side”.
-    Description: “A platform for sharing ghostly encounters”.
-
-2. Enable modular js (in package.json).
-
-hint.md for help
-*/
-/*
-Challenge: 
-
-1.Set up a server that serves the string 
-    '<html><h1>The server is working</h1></html>'.
-    
-    What should the content type be? 
-    What status code should you send?
-
-2. Listen on port 8000 and log a connection message to the console.
-
-3. Open the browser to see your first served HTML.
-
-*/
-
 import http from 'node:http'
+import { serveStatic } from './utils/serveStatic.js'
 
 const PORT = 8000
 
+/*
+Challenge 1:
+
+1. Get the name of the directory holding this server.js file and store it to a const ‘__dirname’.
+*/
+const __dirname = import.meta.dirname
+
 const server = http.createServer((req, res) => {
+
+    /*
+    Challenge 3:
+
+    1. Import and call serveStatic and pass it the directory of this current module.
+    */
+   serveStatic(__dirname)
 
     res.setHeader('Content-Type', 'text/html')
     res.statusCode = 200
