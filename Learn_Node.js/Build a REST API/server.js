77980191588c2a5b1a5509f351a32ba2369ad554
catay-strk <catay.stark@gmail.com>
2026-09-09 14:50:29 +0200

    Get the Query Parameters

--- Learn_Node.js/Build a REST API/server.js
+++ Learn_Node.js/Build a REST API/server.js
@@ -5,23 +5,35 @@ import { filterData } from './utils/getDataByPathParams.js'
 
 const PORT = 8000
 
-/*
-Challenge:
-  1. Add an 'api/country/<country>' route.
-*/
-/*
-Challenge:
-  1. Create a util function to filter data.
-  2. Wire it up and delete unneeded code.
-*/
-
 const server = http.createServer(async (req, res) => {
 
     const destinations = await getDataFromDB()
 
-    if (req.url === "/api" && req.method === "GET") {
+    /*
+        Challenge:
+        1. Complete the two lines of code below.
+            hint.md for help!
+    */
 
-        sendJSONResponse(res, 200, destinations)
+    const urlObj = new URL(req.url, `http://${req.headers.host}`) // Use the URL constructor and pass in the relative and base urls.
+    console.log(urlObj)
+    const queryObj = Object.fromEntries(urlObj.searchParams)// Use the fromEntries() method on the Object class .
+                    // What do you need to pass in? 
+
+    /*
+    Challenge:
+    1. Have a look through the urlObj and find a property which we 
+        can use instead of req.url. We need something that will 
+        satisfy the condition regardless of whether query params were used.
+    */
+
+    if (urlObj.pathname === "/api" && req.method === "GET") {
+        let filteredDestination = destinations
+
+        console.log(queryObj)
+        // update filteredDestinations
+
+        sendJSONResponse(res, 200, filteredDestination)
 
     } else if (req.url.startsWith("/api/continent") && req.method === "GET") {
 
