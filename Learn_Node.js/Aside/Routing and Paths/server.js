60790b8f3c4b4e74b9a0981ec160d84779dff734
catay-strk <catay.stark@gmail.com>
2026-09-09 18:04:25 +0200

    Aside: Serve Multiple Assets

--- Learn_Node.js/Aside/Routing and Paths/server.js
+++ Learn_Node.js/Aside/Routing and Paths/server.js
@@ -1,6 +1,7 @@
 import http from 'node:http'
 import path from 'node:path'
 import fs from 'node:fs/promises'
+import { getContentType } from './utils/getContentType.js'
 
 const PORT = 8000
 
@@ -8,12 +9,19 @@ const __dirname = import.meta.dirname
 
 const server = http.createServer(async (req, res)=> {
 
-  const pathToResource = path.join(__dirname, 'public', 'index.html')
+  const publicDir = path.join(__dirname, 'public')
+  const pathToResource = path.join(
+    publicDir, 
+    req.url === '/' ? 'index.html' : req.url)
 
   const content = await fs.readFile(pathToResource)
 
+  const ext = path.extname(pathToResource)
+
+  const contentType = getContentType(ext)
+
   res.statusCode = 200 
-  res.setHeader('Content-Type', 'text/html')
+  res.setHeader('Content-Type', contentType)
   res.end(content)
 
 })
