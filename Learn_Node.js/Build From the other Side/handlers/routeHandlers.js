492817bda386778c750c0765aa866041a419a2db
catay-strk <catay.stark@gmail.com>
2026-09-10 16:21:21 +0200

    Intro to Nodemon

--- Learn_Node.js/Build From the other Side/handlers/routeHandlers.js
+++ Learn_Node.js/Build From the other Side/handlers/routeHandlers.js
@@ -31,31 +31,13 @@ export async function handlePost(req, res) {
 export async function handleNews(req, res) {
     res.statusCode = 200
 
-    /*
-    Challenge 1:
-    1. Set Content-Type, Cache-Control, and Connection headers
-    */
     res.setHeader("Content-Type", "text/event-stream")
     res.setHeader("Cache-Control", "no-cache")
     res.setHeader("Connection", "keep-alive")
 
     setInterval(() => {
         let randomIndex = Math.floor(Math.random() * stories.length)
-
-        /*
-        Challenge 2:
-        1. Use res.write() to send an object to the frontend. 
-
-        The object should include:
-        - an event property with a descriptive name.
-        - a story chosen at random from the stories array.
-
-        Remember, the object is contained in a string which starts with 'data: '. 
-        What do you need at the end of the string to signal the end of a message block?
-
-        */
         
-        console.log("Header before write: ", res.getHeaders())
         res.write(
             `data: ${JSON.stringify({
                 event: 'news-update',
